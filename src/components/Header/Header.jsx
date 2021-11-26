7c0943d6fd92aa86d67549dd6ff95b5c0205c2f9
JadenXmith <jadenxmith@gmail.com>
2021-11-26 14:16:28 +0000

    Nard Hunt App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import { createMuiTheme, MenuItem, TextField, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+import './header.css';
+import Category from '../../data/category';
+
+const Header = ({ category, setCategory, word, setWord, lightMode}) => {
+    const darkTheme = createMuiTheme({
+        palette: {
+            primary: {
+                main: lightMode ? "#009688" : "#fff"
+            },
+            type: lightMode ? "light" : "dark",
+        },
+    });
+
+    const handleChange = (language) => {
+        setCategory(language)
+        setWord('')
+    }
+    return (
+        <div className="header">
+            <span className="title">{word ? word : "Nard Word Hunt"}</span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField  
+                        label="Search a Word..."
+                        className="search"
+                        value={word}
+                        onChange={e => setWord(e.target.value)}
+                    />
+                    <TextField
+                        className="select"
+                        select 
+                        label="language"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}
+                    >
+                        
+                        {Category.map((option) => (
+                            <MenuItem key={option.label} value={option.label}>{option.value}</MenuItem>
+                        ))}
+                        
+                    </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/definitions/Definitions.jsx
@@ -0,0 +1,51 @@
+import "./definitions.css"
+
+const Definitions = ({ word, category, meanings, lightMode  }) => {
+    return (
+        <div className="meanings">
+            {
+                meanings[0] && word && category === 'en' && (
+                    <audio 
+                        src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} 
+                        style={{backgroundColor: "#fff", borderRadius: '10px'}}
+                        controls    
+                    >
+                        YOur Browser does not support audio element.
+                    </audio>
+                )
+            }
+            { 
+                word === "" ? <span className="subTitle">Start by typing a word in Search</span> : (
+                    meanings.map((mean) => (
+                        mean.meanings.map((item) => (
+                            item.definitions.map((def) => (
+                                <div className="singleMean" style={{backgroundColor: lightMode ? "#3b5360" : 'white', color: lightMode ? "white" : 'black'}}>
+                                    <b>{def.definition}</b>
+                                    <hr style={{backgroundColor: 'black', width: '100%'}} />
+                                    {
+                                        def.example && (
+                                            <span>
+                                                <b>Example: </b>
+                                                {def.example}
+                                            </span>
+                                        )
+                                    }
+                                    {
+                                        def.synonyms && (
+                                            <span>
+                                                <b>Synonyms: </b>
+                                                {def.synonyms.map((s) => `${s}, `)}
+                                            </span>
+                                        )
+                                    }
+                                </div>
+                            ))
+                        ))
+                    ))
+                )
+            }
+        </div>
+    )
+}
+
+export default Definitions
